b2e0d5f410a68b96762aba5368f2819acfea97a5
suzukibelltree <suzuki879455@gmail.com>
2025-08-31 13:33:13 +0900

    Firebase登録用のjsonファイルを追加、各種依存関係を記述

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +50,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // For firebase
+    implementation(libs.firebase.bom)
+    // For firestore
+    implementation(libs.firebase.firestore)
+    // For firebase auth
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.ui.auth)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
