04192652ad03d9e394e26917abfee53d19d61a2c
suzukibelltree <suzuki879455@gmail.com>
2025-08-31 12:42:49 +0900

    パッケージ名を変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pomodoroshareapp"
+    namespace = "com.belltree.pomodoroshareapp"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.pomodoroshareapp"
+        applicationId = "com.belltree.pomodoroshareapp"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
--- app/src/main/java/com/belltree/pomodoroshareapp/ui/theme/Theme.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.example.pomodoroshareapp.ui.theme
+package com.belltree.pomodoroshareapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
