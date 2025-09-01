eb3890cdb225288a91f6c7dd5138cb1f457fa909
carbood <168190155+Back370@users.noreply.github.com>
2025-09-01 15:22:49 +0900

    FloatActionButtonの追加

--- app/src/main/java/com/belltree/pomodoroshareapp/Home/HomeScreen.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/Home/HomeScreen.kt
@@ -1,18 +1,23 @@
 package com.belltree.pomodoroshareapp.Home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.belltree.pomodoroshareapp.ui.components.AppTopBar
 
 @Composable
@@ -30,14 +35,17 @@ fun HomeScreen(
 			)
 		}
 	) {innerPadding ->
-		Column(
-			modifier = modifier
+		Box(
+			modifier = Modifier
 				.fillMaxSize()
-				.padding(innerPadding),
-			horizontalAlignment = Alignment.CenterHorizontally,
-			verticalArrangement = Arrangement.Center
-		) {
-			Text("Home", style = MaterialTheme.typography.headlineMedium)
+				.padding(innerPadding)
+		){
+			FloatingActionButton(
+				modifier = Modifier
+					.align(Alignment.BottomEnd)
+					.padding(12.dp),
+				onClick = { /*TODO*/ }
+			) { Text(text = "+", fontSize = 24.sp, fontWeight = FontWeight.Bold) }
 		}
 	}
 }
