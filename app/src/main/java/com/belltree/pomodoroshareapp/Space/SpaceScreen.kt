07c1d2ad057179f6e440c73c10218f9abcfd0cfe
carbood <168190155+Back370@users.noreply.github.com>
2025-09-01 17:06:32 +0900

    簡易SpaceUI

--- app/src/main/java/com/belltree/pomodoroshareapp/Space/SpaceScreen.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/Space/SpaceScreen.kt
@@ -1,4 +1,34 @@
 package com.belltree.pomodoroshareapp.Space
 
-class SpaceScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.belltree.pomodoroshareapp.ui.components.AppTopBar
+
+@Composable
+fun RecordScreen(
+    modifier: Modifier = Modifier,
+    onNavigateHome: () -> Unit = {}
+) {
+    Scaffold(
+        topBar = {
+            AppTopBar(
+                title = "Space",
+                navigationIcon = Icons.AutoMirrored.Filled.ArrowBack,
+                onNavigationClick = onNavigateHome,
+            )
+        }
+    ) {innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ){
+        }
+    }
 }
