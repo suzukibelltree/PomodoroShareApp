93b9f3ff59964c96312239d688ed818e77911db5
suzukibelltree <suzuki879455@gmail.com>
2025-09-01 16:54:27 +0900

    Recordクラスの構造を変更、RecordRepositoryを実装

--- app/src/main/java/com/belltree/pomodoroshareapp/domain/models/Record.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/domain/models/Record.kt
@@ -1,19 +1,19 @@
 package com.belltree.pomodoroshareapp.domain.models
 
 data class Record(
-    val recordId: String, // RecordのID(Firestore側ではドキュメントID)
+    val recordId: String = "", // RecordのID(Firestore側ではドキュメントID)
+
+    val userId: String = "", // ユーザーID(Firebase AuthenticationのUID)
     // 部屋に参加した情報
-    val roomId: String, // Firestore上のSpaceドキュメントIDと紐づける
-    val roomName: String?,
+    val roomId: String = "", // Firestore上のSpaceドキュメントIDと紐づける
+    val roomName: String? = null, // 参加した部屋の名前
 
     // セッション情報
-    val startTIme: Long, // セッション開始時刻 (UnixTime/ミリ秒)
-    val endTime: Long, // セッション終了時刻 (UnixTime/ミリ秒)
-    val durationMinutes: Int, // 作業時間 (分)
-
-    // 作業内容
-    val taskDescription: List<String?>, // 「何をやっていたか」コメント（1セッションごとに記録）
+    val startTime: Long = 0L, // セッション開始時刻 (UnixTime/ミリ秒)
+    val endTime: Long = 0L, // セッション終了時刻 (UnixTime/ミリ秒)
+    val durationMinutes: Int = 0, // 作業時間 (分)
+    val taskDescription: List<String?> = emptyList(), //作業内容（1セッションごとに記録）
 
     // メタ情報
-    val createdAt: Long // レコード作成時刻（後から並び替えしやすい）
+    val createdAt: Long = 0L // レコード作成時刻（後から並び替えしやすい）
 )
--- app/src/main/java/com/belltree/pomodoroshareapp/domain/repository/RecordRepository.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/domain/repository/RecordRepository.kt
@@ -1,4 +1,34 @@
 package com.belltree.pomodoroshareapp.domain.repository
 
+import com.belltree.pomodoroshareapp.domain.models.Record
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
+
 class RecordRepository {
+    val db = Firebase.firestore
+
+    // そのユーザーの全レコードを取得(RecordViewModelで使用する)
+    suspend fun getAllRecords(userId: String): List<Record> {
+        val snapshot = db.collection("records")
+            .whereEqualTo("userId", userId)
+            .get()
+            .await()
+        return snapshot.documents.mapNotNull { it.toObject(Record::class.java) }
+    }
+
+    // 1セクション目終了時に新しくレコードを保存(SpaceViewModelで使用する)
+    fun addRecord(record: Record) = db.collection("records").add(record)
+
+    // 2セクション目行以降に既存のレコードを更新(SpaceViewModelで使用する)
+    fun updateRecord(recordId: String, updatedData: Record) =
+        db.collection("records").document(recordId).update(
+            mapOf(
+                "endTime" to updatedData.endTime,
+                "durationMinutes" to updatedData.durationMinutes,
+                "taskDescription" to updatedData.taskDescription
+            )
+        )
+
+
 }
