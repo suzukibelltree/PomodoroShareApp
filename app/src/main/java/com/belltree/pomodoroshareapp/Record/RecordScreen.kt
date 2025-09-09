12267cf6ff87d76d347a03f281cd82526f4a6e16
suzukibelltree <suzuki879455@gmail.com>
2025-09-09 21:46:05 +0900

    Geminiを呼び出すところに通常のテキストを差し込み＋アプリアイコン設定

--- app/src/main/java/com/belltree/pomodoroshareapp/Record/RecordScreen.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/Record/RecordScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.belltree.pomodoroshareapp.ui.components.AppTopBar
@@ -30,6 +31,9 @@ fun RecordScreen(
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
+            Text(
+                text = "ここにGeminiのレポートを表示する予定"
+            )
         }
     }
 }
