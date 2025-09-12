d9ffe7ee121baf5d0a930527a0541de40799e463
suzukibelltree <suzuki879455@gmail.com>
2025-09-13 00:38:40 +0900

    アプリ内で使う色を設定した

--- app/src/main/java/com/belltree/pomodoroshareapp/ui/theme/Color.kt
+++ app/src/main/java/com/belltree/pomodoroshareapp/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object PomodoroAppColors {
+    val CoralOrange = Color(0xFFF17650)
+    val LimeGreen = Color(0xFFC9E472)
+    val SkyBlue = Color(0xFF8ECFE3)
+    val TurquoiseBlue = Color(0xFF48B3D3)
+}
